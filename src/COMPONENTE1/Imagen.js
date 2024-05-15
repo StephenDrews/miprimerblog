7f079c168a6f71c0377e28cc12e12c52e5629ab8
Stephen Drews <camilitamikan@gmail.com>
2024-05-15 17:07:19 -0500

    primer commit

---
+++ src/COMPONENTE1/Imagen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Prueba from '../assets/img/imagen1.jpeg';
+
+function Imagen() {
+  return (
+    <div className='Imagenuno'>
+        <div className='Prueba'>
+        <img src={Prueba} alt='' width= {300}/>
+        </div>
+    
+    </div>
+
+  )
+}
+
+export default Imagen
---
+++ src/COMPONENTE1/Pie.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../COMPONENTE1/pie.css'
+
+function Pie() {
+  return (
+    <div className='footer'>
+    &copy; Camila Mikan
+    </div>
+  )
+}
+
+export default Pie
